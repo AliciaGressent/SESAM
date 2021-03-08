5fe11e407946acdaa345493e17d7c21d16c18c72
GRESSENT <51783174+AliciaGressent@users.noreply.github.com>
2021-03-08 16:34:28 +0100

    Add files via upload

---
+++ 03_Mapping/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#MSUB -r mapping_EDK
+#MSUB -Q long
+#MSUB -c 24
+#MSUB -n 2
+#MSUB -q broadwell
+#MSUB -T 172800
+#MSUB -o mapping_urban_scale_EDK_%I.o
+#MSUB -e mapping_urban_scale_EDK_%I.e
+#MSUB -A inerdrc
+
+Rlog=/ccc/work/cont004/ineris/gressena/microcapteurs_carto/MAPPING_SENSOR_DIR/03_Mapping/mapping_urban_scale_EDK.Rlog
+
+module load gnu
+
+R CMD BATCH --no-save --no-restore mapping_urban_scale_EDK.r $Rlog 
+
